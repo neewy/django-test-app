75519d6450ed7f3e3dad607f5344b47ef25b3a5e
Nikolay Yushkevich <n.yushkevich@hotmail.com>
2016-09-14 10:01:38 +0300

    static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+https://github.com/neewy/django-test-app.git
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
